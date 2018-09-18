9595715d5f7c9a347c99817b6afdd2823c6cf74d
howesdomo <164572429@qq.com>
2018-09-18 17:40:04 +0800

    计划做一个双色球机选小程序

---
+++ Client/Client/View/ShuangSeQiu/PageShuangSeQiu.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Client.View.ShuangSeQiu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageShuangSeQiu : ContentPage
+    {
+        public PageShuangSeQiu()
+        {
+            InitializeComponent();
+            initUI();
+        }
+
+        private void initUI()
+        {
+            for (int i = 0; i < 33; i++)
+            {
+                UcShuangSeQiu toAdd_Child = new UcShuangSeQiu();
+                gRedBalls.Children.Add(toAdd_Child);
+            }
+
+            for (int i = 0; i < 15; i++)
+            {
+                UcShuangSeQiu toAdd_Child = new UcShuangSeQiu();
+                gBlueBalls.Children.Add(toAdd_Child);
+            }
+        }
+    }
+}

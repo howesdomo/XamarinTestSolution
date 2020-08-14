0d148d2332679f693cf113b07a0633b007538a42
howesdomo <164572429@qq.com>
2020-08-14 15:30:56 +0800

    优化 FontAwesome 搜索界面, 可以用列表方式 和 图片墙方式展示

--- Client/Client/View/XF_4p6_Features/PageFontAwesome.xaml.cs
+++ Client/Client/View/XF_4p6_Features/PageFontAwesome.xaml.cs
@@ -25,26 +25,20 @@ namespace Client.View.XF_4p6_Features
 
         public PageFontAwesome_ViewModel()
         {
-            this.CMD_Filter = new Command<string>((args) =>
-            {
-                FiltedList.Clear();
 
-                string inputValue = args;
 
-                if (inputValue.IsNullOrWhiteSpace() == false)
-                {
-                    FiltedList = this.AllList.Where(i => new Regex(inputValue, RegexOptions.IgnoreCase).IsMatch(i.Name) == true)
-                                             .ToList();
-                }
-                else
-                {
-                    FiltedList = this.AllList.ToList();
-                }
-            });
+            initCommand();
 
             AllList = Util_Font.FontAwesomeModel.GetAllList();
             FiltedList = new List<Util_Font.FontAwesomeModel>();
             FiltedList.AddRange(AllList);
+        }
+
+        void initCommand()
+        {
+            this.CMD_Filter = new Command<string>((args) => filter(args));
+            this.CMD_ViewOfList = new Command<DataTemplate>(viewOfList);
+            this.CMD_ViewOfGrid = new Command<DataTemplate>(viewOfGrid);
         }
 
         public List<Util_Font.FontAwesomeModel> _FiltedList;
@@ -68,5 +62,105 @@ namespace Client.View.XF_4p6_Features
                 SetProperty(ref _AllList, value);
             }
         }
+
+        void filter(string args)
+        {
+            FiltedList.Clear();
+
+            string inputValue = args;
+
+            if (inputValue.IsNullOrWhiteSpace() == false)
+            {
+                FiltedList = this.AllList.Where(i => new Regex(inputValue, RegexOptions.IgnoreCase).IsMatch(i.Name) == true)
+                                         .ToList();
+            }
+            else
+            {
+                FiltedList = this.AllList.ToList();
+            }
+        }
+
+        private Util_Font.FontAwesomeModel _SelectedItem;
+        public Util_Font.FontAwesomeModel SelectedItem
+        {
+            get { return _SelectedItem; }
+            set
+            {
+                _SelectedItem = value;
+                this.OnPropertyChanged();
+
+                if (value != null && this.ItemsLayout == ItemLayoutOfGrid)
+                {
+                    Device.BeginInvokeOnMainThread(async() =>
+                    {
+                        Acr.UserDialogs.UserDialogs.Instance.Toast(value.Name);
+                        try
+                        {
+                            await Xamarin.Essentials.Clipboard.SetTextAsync(value.Name);
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Diagnostics.Debug.WriteLine(ex.GetInfo());
+                            System.Diagnostics.Debugger.Break();
+                        }
+                    });
+                }
+            }
+        }
+
+
+        public Command<DataTemplate> CMD_ViewOfList { get; private set; }
+
+        public Command<DataTemplate> CMD_ViewOfGrid { get; private set; }
+
+        private DataTemplate _ItemTemplate;
+
+        public DataTemplate ItemTemplate
+        {
+            get { return _ItemTemplate; }
+            set
+            {
+                _ItemTemplate = value;
+                this.OnPropertyChanged("ItemTemplate");
+            }
+        }
+
+
+        void viewOfList(DataTemplate dt)
+        {
+            this.ItemTemplate = dt;
+            this.ItemsLayout = ItemLayoutOfList;
+        }
+
+        void viewOfGrid(DataTemplate dt)
+        {
+            this.ItemTemplate = dt;
+            this.ItemsLayout = ItemLayoutOfGrid;
+        }
+
+
+        private static readonly IItemsLayout ItemLayoutOfGrid = new GridItemsLayout(ItemsLayoutOrientation.Vertical)
+        {
+            Span = 4,
+            VerticalItemSpacing = 20
+        };
+
+        private static readonly IItemsLayout ItemLayoutOfList = new LinearItemsLayout(ItemsLayoutOrientation.Vertical)
+        {
+            ItemSpacing = 5
+        };
+
+        private IItemsLayout _ItemsLayout = ItemLayoutOfList;
+
+        public IItemsLayout ItemsLayout
+        {
+            get { return _ItemsLayout; }
+            set
+            {
+                _ItemsLayout = value;
+                this.OnPropertyChanged();
+            }
+        }
+
     }
 }
